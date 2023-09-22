62c4e4fea0fed4806c51bfb5532f41eed1b41b67
partho0011 <141584603+partho0011@users.noreply.github.com>
2023-09-23 03:51:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 
 
+
+
+
+
 import os
 
-os.system('clear')
-os.system('git --version')
+
+def checkUpdate():
+	os.system("clear");
+	os.system('cd..  && rm -rf Python')
+	os.system("git clone 'https://github.com/partho0011/Python' && cd Python && python main.py")
+	
+	
+
+def main():
+	checkUpdate()
+	
+	print("This is a line")
+	
+	
+main()
